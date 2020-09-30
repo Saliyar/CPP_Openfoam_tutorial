85b0cbcc0f7349951a40a6f0cfc4362721a33774
Sithikaliyar1 <sithik.aliyar@ec-nantes.fr>
2020-09-30 11:59:22 +0200

    Array introduction and calulcation of vector length

---
+++ Arrays/vector_add_Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+double vector_length(double x,double y,double z);
+
+int main()
+{
+    double l;
+    double vector[3][1];
+
+    
+    std::cout<<" Enter the value of x: "<<std::endl;
+    std::cin>>vector[0][0];
+    std::cout<<"Enter the value of y: "<<std::endl;
+    std::cin>>vector[1][0];
+    std::cout<<"Enter the value of z: "<<std::endl;
+    std::cin>>vector[2][0];
+    
+   
+    std::cout<<"The length is: "<<std::endl;
+    l=vector_length(vector[0][0],vector[1][0],vector[2][0]);
+    
+    std::cout<<l<<std::endl;
+    
+    return 0;
+}
+
+double vector_length(double x,double y,double z)
+{
+    double l;
+    l=sqrt(pow(x,2.0)+pow(y,2.0)+pow(z,2.0));
+    return l;
+}
